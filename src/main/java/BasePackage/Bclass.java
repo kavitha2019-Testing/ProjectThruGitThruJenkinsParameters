f7e809633654be6a5a8227199e869e45438f5e39
kavitha <kavitha.abburi@gmail.com>
2020-10-01 13:11:45 +0100

    firstcommit(1/10)

---
+++ src/main/java/BasePackage/Bclass.java
@@ -0,0 +1,64 @@
+package BasePackage;
+
+import java.util.concurrent.TimeUnit;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+public class Bclass {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void browseropen() {
+        if (System.getProperty("browser").equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.setPageLoadStrategy(PageLoadStrategy.NONE);
+            options.addArguments("enable-automation");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-infobars");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-browser-side-navigation");
+            options.addArguments("--disable-gpu");
+            driver = new ChromeDriver(options);
+        } else if (System.getProperty("browser").equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions options = new FirefoxOptions();
+            options.setPageLoadStrategy(PageLoadStrategy.NONE);
+            options.addArguments("enable-automation");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-infobars");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-browser-side-navigation");
+            options.addArguments("--disable-gpu");
+            driver = new FirefoxDriver(options);
+        } else if (System.getProperty("browser").equalsIgnoreCase("IE")) {
+            WebDriverManager.iedriver().setup();
+            InternetExplorerOptions options = new InternetExplorerOptions();
+            options.setPageLoadStrategy(PageLoadStrategy.NONE);
+//                                  options.addArguments("enable-automation");
+//                                  options.addArguments("--no-sandbox");
+//                                   options.addArguments("--disable-infobars");
+//                                   options.addArguments("--disable-dev-shm-usage");
+//                                   options.addArguments("--disable-browser-side-navigation");
+//                                   options.addArguments("--disable-gpu");
+           driver = new InternetExplorerDriver(options);
+       }
+            driver.get("https://www.hotukdeals.com/?page=4");
+            driver.manage().window().maximize();
+            driver.manage().deleteAllCookies();
+            driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.MILLISECONDS);
+            driver.manage().timeouts().implicitlyWait(60, TimeUnit.MILLISECONDS);
+        }
+        @AfterMethod
+    public void teardown() {
+        driver.close();
+    }
+}
---
+++ src/test/java/TestCases/TCase1.java
@@ -0,0 +1,23 @@
+package TestCases;
+
+import BasePackage.Bclass;
+import org.testng.annotations.Test;
+
+public class TCase1  extends Bclass {
+    @Test
+    public void test1(){
+        System.out.println("this is test1");
+    }
+    @Test
+    public void test2(){
+        System.out.println("this is test2");
+    }
+    @Test
+    public void test3(){
+        System.out.println("this is test3");
+    }
+    @Test
+    public void test4() {
+        System.out.println("this is test4");
+    }
+}
